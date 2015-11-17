57d2447d3c22ea609e9df14d794494d0901fe52b
Alexsandro <alexsandro.xpt@gmail.com>
2015-11-17 18:37:55 -0200

    Testes em dx-responsive-box.

--- app/views/login/rota.js
+++ app/views/login/rota.js
@@ -8,7 +8,7 @@ module.exports = ['login', {
 		return $q(function (resolve) {
 			// lazy load the view
 			require.ensure([], function () {
-				resolve(require('./' + view + '3.html'));
+				resolve(require('./' + view + '2.html'));
 			});
 		});
 	},
